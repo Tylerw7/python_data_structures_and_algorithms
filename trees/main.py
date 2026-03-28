d755390ac076aab514afa07d52e67844eaae49d9
Tylerw7 <t4wettach@gmail.com>
2026-03-28 09:03:28 -0400

    update

--- trees/main.py
+++ trees/main.py
@@ -3,6 +3,8 @@
 # Trees Notes
 # A child node that doesn'y have any children is called a LEAF
 
+# BINARY SEARCH TREE: numbers greater than parent node goes on the right, less than goes on the left
+
 
 class Node:
     def __init__(self, value):
