ee9d9520521a93e1f4d77f2d40d6663968bfba3c
Tylerw7 <t4wettach@gmail.com>
2026-03-28 08:39:58 -0400

    added tree folder

---
+++ trees/main.py
@@ -0,0 +1,13 @@
+
+
+# Trees Notes
+# A child node that doesn'y have any children is called a LEAF
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
